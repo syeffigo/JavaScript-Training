36f686cbf66ff0d52381a438f25a2fc82f51e577
syeffigo <saurabh.yadav@effigo.tech>
2024-02-21 11:39:59 +0530

    Function examples added

---
+++ Basics/Functions.js
@@ -0,0 +1,16 @@
+function myFunction(a, b){
+    return a*b;
+}
+
+let x = myFunction(4,3);
+console.log(x);
+
+
+
+function toCelsius(fahrenheit){
+    return (5/9)*(fahrenheit-32);
+}
+
+let value = toCelsius(56);
+console.log(value);
+
